067c7e6798712129649288f9161f024e6d29eb58
vijayachitrabio <vijayachitra.m@gmail.com>
2016-06-10 10:02:21 +0300

    Add files via upload

---
+++ submit_singlenode_rocket_serial.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J binning_serial 
+#SBATCH -N 1 
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 00:05:00
+
+module purge
+module load gcc-4.8.1
+module load zlib-1.2.8
+export MPIDIR=OpenMPI
+export RDIR=R_directory
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/$RDIR/R300install/lib64/R/lib
+export MPIINCLUDE=$HOME/$MPIDIR/install/include
+export MPILIB=$HOME/$MPIDIR/install/lib
+export OMP_NUM_THREADS=1
+
+~/OpenMPI/install/bin/mpirun  ~/R_directory/R300install/bin/Rscript --vanilla  ./pca_demo.R
+
